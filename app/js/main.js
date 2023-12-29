58029007df2803c969e37cc04a19051c281b0a4c
ITfrontTop <Fine1337@ukr.net>
2023-12-29 20:21:35 +0200

    added index and proof of stake page html

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// slider
 const sliderMain = new Swiper('.header__content-container', {
     // effect: 'coverflow',
     // depth: 100,
@@ -18,4 +19,74 @@ const sliderMain = new Swiper('.header__content-container', {
     },
 
 });
+
+
+// увеличения цифр
+// const time = 3000;
+// const step = 1;
+
+// function outNum(num, elem) {
+//     let e = document.querySelector(`${elem}`);
+//     n = 0;
+//     let t = Math.round(time / (num / step));
+//     let interval = setInterval(() => {
+//         n = n + 5;
+//         if (n === num) {
+//             clearInterval(interval);
+//         }
+//         e.innerHTML = n;
+//     }, t)
+// }
+
+// outNum(10, '.header__well-name');
+
+
+// faq section
+function resetAccord() {
+    faqTitles.forEach(item => {
+        item.classList.remove('open');
+    })
+    faqContents.forEach(item => {
+        item.classList.remove('open');
+    })
+}
+
+// questions
+const faqQuestions = document.querySelectorAll('.faq__questions-finance-box');
+const faqTitles = document.querySelectorAll('.faq__question-finance-title');
+const faqContents = document.querySelectorAll('.faq__question-finance-content');
+
+
+faqQuestions.forEach((item) => {
+    const questionFinanceTitle = item.querySelector('.faq__question-finance-title');
+    const questionFinanceContent = item.querySelector('.faq__question-finance-content');
+
+    questionFinanceTitle.addEventListener('click', () => {
+        resetAccord();
+        questionFinanceTitle.classList.toggle('open');
+        questionFinanceContent.classList.toggle('open');
+    })
+})
+
+// categories item
+const faqItems = document.querySelectorAll('[data-tab]');
+const faqContent = document.querySelectorAll('[data-tab-content]');
+
+faqItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+
+        resetAccord();
+
+        faqItems.forEach((item) => {
+            item.classList.remove('faq__categories-item--active');
+        })
+        item.classList.add('faq__categories-item--active');
+
+        faqContent.forEach((item) => {
+            item.classList.add('hidden');
+        })
+        const contentBox = document.querySelector('#' + this.dataset.tab)
+        contentBox.classList.remove('hidden')
+    })
+})
 
